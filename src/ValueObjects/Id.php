0cae75952a4ff6498ca130a12be2334a81c8ed3f
Konstantin Aleksandrov <k.aleksandrov@action-media.ru>
2020-01-21 22:59:34 +0300

    id value object

---
+++ src/ValueObjects/Id.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jc9Group\DataStructures\ValueObjects;
+
+class Id implements \JsonSerializable
+{
+    /**
+     * @var string
+     */
+    private $value;
+
+    public function __construct(string $value)
+    {
+        if (empty($value)) {
+            throw new \InvalidArgumentException('ID value must not be empty');
+        }
+
+        $this->value = $value;
+    }
+
+    public function jsonSerialize(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+    
+    public function isEqual(Id $id): bool
+    {
+        return $this->value === (string)$id;
+    }
+}
